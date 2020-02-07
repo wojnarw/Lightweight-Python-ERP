17af9757c7305a7f066f1e7c38fbb4f960534650
Justyna Andrzejak <justyna.andrzejak1@gmail.com>
2020-02-07 09:34:02 +0100

    Update accounting and crm

--- accounting/accounting.py
+++ accounting/accounting.py
@@ -18,81 +18,97 @@ import data_manager
 # common module
 import common
 
+items_list = []
+repeat = True
+month_index, day_index, year_index, in_out_index, amount_index = 1, 2, 3, 4, 5
+
 
 def start_module():
-    """
-    Starts this module and displays its menu.
-     * User can access default special features from here.
-     * User can go back to main menu from here.
 
-    Returns:
-        None
-    """
+    global repeat
 
-    # you code
+    items_list = common.read_from_file_to_table("accounting/items.csv")
+
+    while repeat:
+        # display Sales menu
+        options = ["Display list",
+                   "Add new record",
+                   "Remove a record",
+                   "Update specific record",
+                   "Get year with highest profit",
+                   "Get average of year"]
+
+        ui.print_menu("Accounting manager menu",
+                      options, "Return to main menu")
+        try:
+            choose(items_list)
+        except KeyError as err:
+            ui.print_error_message(str(err))
+
+
+def choose(items_list):
+
+    global repeat
+
+    inputs = ui.get_inputs(["Please enter a number: "], "")
+    option = inputs[0]
+    if option == "1":
+        show_table(items_list)
+    elif option == "2":
+        table = ui.get_inputs(["Month: ", "Day: ", "Year: ",
+                               "In or out: ", "Amount "], "Please insert new records")
+        add(table, items_list)
+    elif option == "3":
+        id_ = ui.get_inputs(
+            ["Enter the index of record which you want to remove: "], "")[0]
+        items_list = remove(items_list, int(id_))
+    elif option == "4":
+        id_ = ui.get_inputs(["Please choose index: "], "")[0]
+        items_list = update(items_list, int(id_))
+    elif option == "5":
+        which_year_max(items_list)
+    elif option == "6":
+        year = ui.get_inputs(["Enter year : "], "")[0]
+        avg_amount(items_list, year)
+    elif option == "0":
+        repeat = False
+    else:
+        raise KeyError("There is no such option.")
 
 
 def show_table(table):
-    """
-    Display a table
 
-    Args:
-        table (list): list of lists to be displayed.
-
-    Returns:
-        None
-    """
-
-    # your code
+    title_list = ["ID", "MONTH", "DAY", "YEAR", "IN OR OUT", "AMOUNT"]
+    ui.print_table(table, title_list)
 
 
-def add(table):
-    """
-    Asks user for input and adds it into the table.
+def add(table, items_list):
 
-    Args:
-        table (list): table to add new record to
-
-    Returns:
-        list: Table with a new record
-    """
-
-    # your code
-
-    return table
+    table.insert(0, common.generate_random())
+    items_list.append(table)
+    return items_list
 
 
 def remove(table, id_):
-    """
-    Remove a record with a given id from the table.
 
-    Args:
-        table (list): table to remove a record from
-        id_ (str): id of a record to be removed
-
-    Returns:
-        list: Table without specified record.
-    """
-
-    # your code
-
+    # correct index, so if user entered 1, we remove item with first index [0]
+    table.pop(id_ - 1)
     return table
 
 
 def update(table, id_):
-    """
-    Updates specified record in the table. Ask users for new data.
 
-    Args:
-        table (list): list in which record should be updated
-        id_ (str): id of a record to update
+    # correct index, so if user entered 1, we remove item with first index [0]
+    id_ -= 1
 
-    Returns:
-        list: table with updated record
-    """
+    inputs = ui.get_inputs([f"Month (current: {table[id_][month_index]}): ", f"Day (current: {table[id_][day_index]}): ",
+                            f"Year (current: {table[id_][year_index]}): ", f"In or out (current: {table[id_][in_out_index]}): ",
+                            f"Amount (current: {table[id_][amount_index]}): "], "Please insert new record")
 
-    # your code
-
+    # iterate through the list 1 time less than its length, to ignore unchangeable id
+    for i in range(len(table[id_])-1):
+        # skip first table item, which contains entry unchangeable id
+        table[id_][month_index + i] = inputs[i]
     return table
 
 
@@ -100,29 +116,42 @@ def update(table, id_):
 # ------------------
 
 def which_year_max(table):
-    """
-    Question: Which year has the highest profit? (profit = in - out)
 
-    Args:
-        table (list): data table to work on
+    in_and_out = {}
+    best_year = 0
 
-    Returns:
-        number
-    """
+    for line in table:
+        year = line[3]
+        operation = line[4]
+        amount = line[5]
+        if year not in in_and_out.keys():
+            in_and_out.update({year: 0})
+        if operation == "in":
+            in_and_out[year] = int(in_and_out[year]) + int(amount)
+        if operation == "out":
+            in_and_out[year] = int(in_and_out[year]) - int(amount)
 
-    # your code
+    max_key = max(in_and_out, key=lambda k: in_and_out[k])
+    ui.print_result(max_key, "Year which has the highest profit is ")
+    return max_key
 
 
 def avg_amount(table, year):
-    """
-    Question: What is the average (per item) profit in a given year? [(profit)/(items count)]
+    profit = 0
+    items_count = 0
+    for line in table:
+        operation = line[4]
+        amount = line[5]
+        if line[3] == year:
+            items_count += 1
+            if operation == "in":
+                profit += int(amount)
+            if operation == "out":
+                profit -= int(amount)
+    ui.print_result(str(items_count), " items ciunt ")
+    average = profit/items_count
 
-    Args:
-        table (list): data table to work on
-        year (number)
+    ui.print_result(str(average), "Year " + year +
+                    " has the avarage profit is ")
 
-    Returns:
-        number
-    """
-
-    # your code
+    return average
--- crm/crm.py
+++ crm/crm.py
@@ -16,64 +16,105 @@ import data_manager
 # common module
 import common
 
+# def import_text(filename="text_albums_data.txt"):
+#  global music_data
+# with open(filename, "r", encoding="utf-8") as file_txt:
+#   music_combined = file_txt.readlines()
+# print(music_combined)
+#music_data = [last_character.replace("\n", "") for last_character in music_combined]
+# print(music_data)
+#music_data = [row.split(",") for row in music_data]
+# print(music_data)
+
+customer_list = []
+repeat = True
+name_index, email_index, subscribe_index = 1, 2, 3
+
+
+# def import_text(filename="customers.csv"):
+#   with open(filename, "r") as file_csv:
+#      file_with_customers = file_csv.readlines()
+#     list_with_customers_data = [last_character.replace("\n", "") for last_character in list_with_customers_data]
+#    list_with_customers_data = [row.split(",") for row in list_with_customers_data]
+
 
 def start_module():
-    """
-    Starts this module and displays its menu.
-     * User can access default special features from here.
-     * User can go back to main menu from here.
+    global repeat
 
-    Returns:
-        None
-    """
+    # read from file to 2D table
+    customer_list = common.read_from_file_to_table("crm/customers.csv")
 
-    # your code
+    while repeat:
+        # display Sales menu
+        options = ["Display list",
+                   "Add new record",
+                   "Remove a record",
+                   "Update specific record",
+                   "Get longest name",
+                   "Get subscibers"]
+
+        ui.print_menu("Customer Relationhip manager menu",
+                      options, "Return to main menu")
+        try:
+            choose(customer_list)
+        except KeyError as err:
+            ui.print_error_message(str(err))
+
+
+def choose(customer_list):
+
+    global repeat
+
+    inputs = ui.get_inputs(["Please enter a number: "], "")
+    option = inputs[0]
+    if option == "1":
+        show_table(customer_list)
+    elif option == "2":
+        table = ui.get_inputs(
+            ["Name ", "Email: ", "Subscibe (yes/no): "], "Please insert new records")
+        add(table, customer_list)
+    elif option == "3":
+        id_ = ui.get_inputs(
+            ["Enter the index of record which you want to remove: "], "")[0]
+        customer_list = remove(customer_list, int(id_))
+        #id_ = ui.get_inputs(["ID: "],"Enter the ID of record which you want to remove")
+        #remove(customer_list, int(id_))
+    elif option == "4":
+        id_ = ui.get_inputs(["Please choose index: "], "")[0]
+        customer_list = update(customer_list, int(id_))
+    elif option == "5":
+        get_longest_name_id(customer_list)
+    elif option == "6":
+        get_subscribed_emails(customer_list)
+    elif option == "0":
+        repeat = False
+    else:
+        raise KeyError("There is no such option.")
 
 
 def show_table(table):
-    """
-    Display a table
 
-    Args:
-        table (list): list of lists to be displayed.
-
-    Returns:
-        None
-    """
+    title_list = ["ID", "NAME", "EMAIL", "SUBSCRIBE"]
+    ui.print_table(table, title_list)
 
     # your code
 
 
-def add(table):
-    """
-    Asks user for input and adds it into the table.
+def add(table, customer_list):
 
-    Args:
-        table (list): table to add new record to
-
-    Returns:
-        list: Table with a new record
-    """
-
-    # your code
-
-    return table
+    if table[2] == "yes":
+        table[2] = "1"
+    else:
+        table[2] = "0"
+    table.insert(0, common.generate_random())
+    customer_list.append(table)
+    return customer_list
 
 
 def remove(table, id_):
-    """
-    Remove a record with a given id from the table.
 
-    Args:
-        table (list): table to remove a record from
-        id_ (str): id of a record to be removed
-
-    Returns:
-        list: Table without specified record.
-    """
-
-    # your code
-
+    # correct index, so if user entered 1, we remove item with first index [0]
+    table.pop(id_ - 1)
     return table
 
 
@@ -89,8 +130,21 @@ def update(table, id_):
         list: table with updated record
     """
 
-    # your code
+    # correct index, so if user entered 1, we remove item with first index [0]
+    id_ -= 1
 
+    inputs = ui.get_inputs([f"Name (current: {table[id_][name_index]}): ", f"Email (current: {table[id_][email_index]}): ",
+                            f"Subscriber (yes/no) (current: {table[id_][subscribe_index]}): "], "Please insert new record")
+
+    if inputs[2] == "yes":
+        inputs[2] = "1"
+    else:
+        inputs[2] = "0"
+
+    # iterate through the list 1 time less than its length, to ignore unchangeable id
+    for i in range(len(table[id_])-1):
+        # skip first table item, which contains entry unchangeable id
+        table[id_][name_index + i] = inputs[i]
     return table
 
 
@@ -98,31 +152,34 @@ def update(table, id_):
 # ------------------
 
 def get_longest_name_id(table):
-    """
-        Question: What is the id of the customer with the longest name?
 
-        Args:
-            table (list): data table to work on
+    list_with_longest = []
+    longest = 0
 
-        Returns:
-            string: id of the longest name (if there are more than one, return
-                the last by alphabetical order of the names)
-        """
+    for line in table:
+        if len(line[1]) > longest:
+            longest = len(line[1])
 
-    # your code
+    for line in table:
+        if len(line[1]) == longest:
+            list_with_longest.append(line)
+
+    lastLongest = list_with_longest[1]
+
+    for line in list_with_longest:
+        if lastLongest[1] < line[1]:
+            lastLongest = line
+    ui.print_result(lastLongest[0], "Id najdłuższego wyrazu")
+    return lastLongest[0]
 
 
-# the question: Which customers has subscribed to the newsletter?
-# return type: list of strings (where string is like email+separator+name, separator=";")
 def get_subscribed_emails(table):
-    """
-        Question: Which customers has subscribed to the newsletter?
 
-        Args:
-            table (list): data table to work on
+    sub_list = []
 
-        Returns:
-            list: list of strings (where a string is like "email;name")
-        """
+    for line in table:
+        if line[3] == "1":
+            sub_list.append(line)
 
-    # your code
+    show_table(sub_list)
+    return sub_list
